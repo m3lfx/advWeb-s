5fcbf4b9ce9c74e2b12446997cb45ea3907ae1af
rommel dalisay <rommelmdalisay@gmail.com>
2024-05-11 11:08:45 +0800

    eager loading multiple relations

--- app/DataTables/AdminOrdersDataTable.php
+++ app/DataTables/AdminOrdersDataTable.php
@@ -13,6 +13,7 @@ use Yajra\DataTables\Html\Editor\Fields;
 use Yajra\DataTables\Services\DataTable;
 use DB;
 use App\Models\Order;
+use Debugbar;
 
 class AdminOrdersDataTable extends DataTable
 {
@@ -24,13 +25,33 @@ class AdminOrdersDataTable extends DataTable
      */
     public function dataTable($query)
     {
-        return datatables()->query($query)
-        ->addColumn('action',  function ($row) {
-            // $actionBtn = '<a href="#"  class="btn details btn-primary">Details</a>';
+        // return datatables()->query($query)
+        // ->addColumn('action',  function ($row) {
+        //     // $actionBtn = '<a href="#"  class="btn details btn-primary">Details</a>';
+        //     $actionBtn = '<a href="' . route('admin.orderDetails', $row->orderinfo_id) . '"  class="btn details btn-primary">Details</a>';
+        //     return $actionBtn;
+        // })->rawColumns(['action'])
+        // ->setRowId('id');
+
+        return datatables()
+        ->eloquent($query)
+        ->addColumn('action',  function($row){
             $actionBtn = '<a href="' . route('admin.orderDetails', $row->orderinfo_id) . '"  class="btn details btn-primary">Details</a>';
+            // $actionBtn = '<a href="#!"  class="btn details btn-primary">Details</a>';
+           
             return $actionBtn;
-        })->rawColumns(['action'])
-        ->setRowId('id');
+        })
+        ->addColumn('total', function ($order) {
+            // DebugBar::info($row);
+            return number_format($order->items->map(function($item) {
+                return  $item->pivot->quantity * $item->sell_price;
+            })->sum(),2);
+        })
+        ->addColumn('items', function ($order) {
+            return  $order->items->map(function($item) {
+                return $item->description;
+            })->implode('<br />');
+        })->rawColumns(['action','items']);
     }
 
     /**
@@ -41,13 +62,16 @@ class AdminOrdersDataTable extends DataTable
      */
     public function query()
     {
-        $orders = DB::table('customer as c')->join('orderinfo as o','o.customer_id', '=', 'c.customer_id')
-        ->join('orderline as ol','o.orderinfo_id', '=', 'ol.orderinfo_id')
-        ->join('item as i','ol.item_id', '=', 'i.item_id')
-        ->select('o.orderinfo_id as orderinfo_id','c.fname', 'c.lname', 'c.addressline', 'o.date_placed', 'o.status', DB::raw("SUM(ol.quantity * i.sell_price) as total"))
-        ->groupBy('o.orderinfo_id', 'o.date_placed','o.orderinfo_id','c.fname', 'c.lname', 'c.addressline', 'o.status');
+        // $orders = DB::table('customer as c')->join('orderinfo as o','o.customer_id', '=', 'c.customer_id')
+        // ->join('orderline as ol','o.orderinfo_id', '=', 'ol.orderinfo_id')
+        // ->join('item as i','ol.item_id', '=', 'i.item_id')
+        // ->select('o.orderinfo_id as orderinfo_id','c.fname', 'c.lname', 'c.addressline', 'o.date_placed', 'o.status', DB::raw("SUM(ol.quantity * i.sell_price) as total"))
+        // ->groupBy('o.orderinfo_id', 'o.date_placed','o.orderinfo_id','c.fname', 'c.lname', 'c.addressline', 'o.status');
         // ->get();
         // dd($orders);
+        $orders = Order::with(['customer','items'])->whereHas('items');
+        $order_items = Order::with(['customer','items'])->whereHas('items')->get();
+        DebugBar::info($order_items);
         return $orders;
     }
 
@@ -78,26 +102,42 @@ class AdminOrdersDataTable extends DataTable
      */
     public function getColumns(): array
     {
+        // return [
+        //     Column::computed('action')
+        //           ->exportable(false)
+        //           ->printable(false)
+        //           ->width(60)
+        //           ->addClass('text-center'),
+        //     // Column::make('orderinfo_id'),
+        //     // Column::make('fname')->title('first name'),
+        //     // Column::make('lname')->title('last name'),
+        //     // Column::make('addressline')->title('address'),
+        //     // Column::make('date_placed'),
+        //     // Column::make('status'),
+        //     ['data' => 'orderinfo_id', 'name' => 'o.orderinfo_id', 'title' => 'order id'],
+        //     ['data' => 'lname', 'name' => 'c.lname', 'title' => 'last name'],
+        //     ['data' => 'fname', 'name' => 'c.fname', 'title' => 'first Name'],
+        //     ['data' => 'addressline', 'name' => 'c.addressline', 'title' => 'address'],
+        //     ['data' => 'date_placed', 'name' => 'o.date_placed', 'title' => 'date ordered'],
+        //     ['data' => 'status', 'name' => 'o.status', 'title' => 'status'],
+        //     Column::make('total')->searchable(false)
+            
+        // ];
+
         return [
+            Column::make('orderinfo_id'),
+            Column::make('customer.fname')->title('first name'),
+            Column::make('customer.lname')->title('last name'),
+            Column::make('customer.addressline')->title('address'),
+            Column::make('date_placed')->title('date ordered'),
+            Column::make('total'),
+            Column::make('status'),
+            Column::make('items'),
             Column::computed('action')
                   ->exportable(false)
                   ->printable(false)
                   ->width(60)
                   ->addClass('text-center'),
-            // Column::make('orderinfo_id'),
-            // Column::make('fname')->title('first name'),
-            // Column::make('lname')->title('last name'),
-            // Column::make('addressline')->title('address'),
-            // Column::make('date_placed'),
-            // Column::make('status'),
-            ['data' => 'orderinfo_id', 'name' => 'o.orderinfo_id', 'title' => 'order id'],
-            ['data' => 'lname', 'name' => 'c.lname', 'title' => 'last name'],
-            ['data' => 'fname', 'name' => 'c.fname', 'title' => 'first Name'],
-            ['data' => 'addressline', 'name' => 'c.addressline', 'title' => 'address'],
-            ['data' => 'date_placed', 'name' => 'o.date_placed', 'title' => 'date ordered'],
-            ['data' => 'status', 'name' => 'o.status', 'title' => 'status'],
-            Column::make('total')->searchable(false)
-            
         ];
     }
 
